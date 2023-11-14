147034ff57dcc9579dac4388194af9c0339da443
lucassouza31 <lucaseasy10@gmail.com>
2023-11-14 18:49:26 -0300

    Projeto Padrões

--- factoryMethod/CriadorUnidade.java
+++ factoryMethod/CriadorUnidade.java
@@ -1,11 +1,19 @@
 package factoryMethod;
 
-public abstract class CriadorUnidade {
+import prototype.Clone;
+
+public abstract class CriadorUnidade extends Clone {
     public abstract Unidade criarUnidade();
 
     public Unidade criarPeca(){
         Unidade unidade=criarUnidade();
         return unidade;
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        //clonar peca
+        return super.clone();
     }  
             
 }
---
+++ prototype/Clone.java
@@ -0,0 +1,5 @@
+package prototype;
+
+public abstract class Clone implements Cloneable {
+    abstract Clone clonarPeca();
+}
